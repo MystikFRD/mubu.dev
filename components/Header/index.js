0c66cc70536b067c2240e5a2e78546b14806c294
Mystik <mustafa.budak@fom-net.de>
2025-02-07 03:37:14 +0100

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -70,19 +70,17 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               {!isBlog ? (
                 <div className="grid grid-cols-1">
                   <Button onClick={handleWorkScroll}>Work</Button>
-                  <Button onClick={handleWorkScroll}>Work</Button>
                   <Button onClick={handleAboutScroll}>About</Button>
                   {showBlog && (
                     <Button onClick={() => router.push("/blog")}>Blog</Button>
                   )}
                   {showResume && (
-                    <Button
-                      onClick={() =>
-                        window.open("mailto:work@mubu.dev")
-                      }
-                    >
-                      Resume
-                    </Button>
+                      <Button
+                          onClick={() => router.push("/resume")}
+                          classes="first:ml-1"
+                      >
+                          Resume
+                      </Button>
                   )}
 
                   <Button
